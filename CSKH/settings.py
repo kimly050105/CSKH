6433577707f3c79a9276b4264b9f51bda73ab643
tranngocdiem020205 <tranngocdiem020205@gmail.com>
2025-10-29 11:07:30 +0700

    Thêm trang Liên hệ + layout chung (header, footer, chatbox)

--- CSKH/settings.py
+++ CSKH/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'TB.context_processors.thong_bao_moi',
 
+
             ],
         },
     },
