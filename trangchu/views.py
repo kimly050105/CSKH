4abef7b1a204fe92137c4f794504a2e6ff5e4211
anh Diên <gucuatuidue@gmail.com>
2025-10-29 14:53:25 +0700

    commit lại

--- trangchu/views.py
+++ trangchu/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'trangchu/index.html')
+
+def lien_he(request):
+    return render(request, 'trangchu/lienhe.html')
+
+
