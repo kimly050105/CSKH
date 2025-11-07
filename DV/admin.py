891278ccf24c37065ee0d334b46a91adc0ffe76f
phuongthao <phuongthaopt2903@gmail.com>
2025-11-08 01:24:37 +0700

    Thêm giao diện và chức năng Dịch vụ (DV)

--- DV/admin.py
+++ DV/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from .models import DichVu
+
+@admin.register(DichVu)
 class DichVuAdmin(admin.ModelAdmin):
     list_display = ('ten_dich_vu', 'gia')
-admin.site.register(DichVu, DichVuAdmin)
 
+    # 🟣 Ẩn quyền xoá cho người không phải superuser
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
 
-
-
-
-# Register your models here.
+    # 🩷 Ẩn luôn hành động "Xóa các dịch vụ đã chọn" khỏi menu hành động
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if not request.user.is_superuser:
+            if 'delete_selected' in actions:
+                del actions['delete_selected']
+        return actions
